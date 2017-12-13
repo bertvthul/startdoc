1c3f6d549b789760d22213e32f4ff4dfb4a020b3
Bert van 't Hul <bertvthul@gmail.com>
2017-12-13 15:38:11 +0100

    eerste versie boilerplate html

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta charset="utf-8">
+	
+	<title>Project A</title>
+	
+	<!-- jquery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	
+	
+	<!-- bootstrap -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	
+	<!-- fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Roboto:300,400,700,900' rel='stylesheet' type='text/css'>
+	
+	<!-- font awsome -->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	
+	<!-- transit: soepele animaties -->
+	<script src="js/jquery.transit.min.js" type="text/javascript"></script>
+	
+	<!-- animatein: elementen animeren wanneer zichtbaar -->
+	<script src="js/animatein.min.js" type="text/javascript"></script>
+	
+	
+	<!-- site -->
+	<link rel="stylesheet" href="css/template.css" type="text/css" />
+	<script src="js/script.js" type="text/javascript"></script>
+	
+    
+</head>
+
+<body>
+	<div id="container">
+		<div id="containerOverlay"></div>
+		<header class="boxShadow">
+			<div class="centered">
+				<div id="logo"><i class="fa fa-diamond"></i> LOGO PROJECT A</div>
+				
+				
+				
+				<div class="mobMenuBtn"><div class="mobMenuLine"></div><div class="mobMenuLine"></div><div class="mobMenuLine"></div><div class="mobMenuText">menu</div></div><!--mobMenuBtn-->
+				
+				
+				<div id="headerRight">
+					
+					
+					
+				</div><!-- headerRight -->
+				
+				
+				
+				<div class="clearfix"></div>
+			</div>
+		</header>
+
+		<nav>
+			<div class="centered">
+				<ul class="menu horizmenu">
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Project</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+		</nav>
+		
+		<div class="centered">
+			<section id="main">
+				
+				
+				<h1>Welkom, dit is project A</h1>
+				<p>Kijk gerust rond en geef feedback</p>
+				
+				<div class="well">
+					<dl class="dl-horizontal">
+					  <dt>Overzichtelijk</dt>
+					  <dd>Duidelijk overzicht</dd>
+					  <dt>Reminder</dt>
+					  <dd>Ontvang emails wanneer er wat moet gebeuren.</dd>
+					  <dt>Persoonlijk</dt>
+					  <dd>Maak een account, geef aan wat je wilt en zie je persoonlijke overzicht.</dd>
+					</dl>
+				</div>
+				
+				<p>a</p>
+				<p>b</p>
+				
+				<div class="clearfix"></div>
+				
+			</section>
+
+			<aside id="right">
+				Content rechts
+			</aside>
+		</div><!-- centered -->
+		
+		
+		<div class="footerHeight"></div>
+	
+	
+		<footer class="footerHeight boxShadow">
+			<div class="centered">
+				<div id="footerInner">
+					Copyright 2015
+				</div>
+				<div class="socialIcons" data-animatein="ttb" data-child=".fa" data-distance="40" data-speed="200" data-delay="100">
+					<a href="#" target="_blank" class="flipper fa fa-facebook hasTooltip" title="Volg ons op facebook"></a> <a href="#" target="_blank" class="flipper fa fa-twitter"></a> <a href="#" target="_blank" class="flipper fa fa-linkedin"></a> <a href="#" target="_blank" class="flipper fa fa-phone"></a>
+				</div>
+			</div>
+		</footer>
+
+	</div><!-- container -->
+	<div id="sideMenu">
+		<div id="sideMenuInner">	
+			<div id="sideMenuTop">
+				<div id="sideMenuHide">×</div>
+				<h3>Menu</h3>
+			</div><!-- #sideMenuTop -->
+			<div id="sideMenuCenter">
+				<div id="sidemenu" class="vertmenu">
+		        	<ul class="nav menu ">
+						<li class=""><a href="#"><span>Home</span></a></li>
+						<li class=""><a href="#"><span>Menu 2</span></a></li>
+						<li class=""><a href="#"><span>Menu 3</span></a></li>
+						<li class=""><a href="#"><span>Menu 4</span></a></li>
+						<li class=""><a href="#"><span>Menu 5</span></a></li>
+					</ul>
+
+		        </div>
+		    </div><!-- #sideMenuCenter -->
+	        
+		</div><!-- #sideMenuInner -->
+	</div><!-- #sideMenu -->
+</body>
+</html>
