27606b17918cd6a911cd64b2eb808e04547f805d
Bert <bertvthul@gmail.com>
2017-12-13 22:07:38 +0100

    Titel bij index aangepast

--- index.php
+++ index.php
@@ -1,60 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	
+
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta charset="utf-8">
-	
+
 	<title>Project A</title>
-	
+
 	<!-- jquery -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	
-	
+
+
 	<!-- bootstrap -->
 	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	
+
 	<!-- fonts -->
 	<link href="//fonts.googleapis.com/css?family=Roboto:400,700|Roboto+Slab:400" rel="stylesheet">
-	
+
 	<!-- font awsome -->
 	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-	
-	
+
+
 	<!-- site -->
 	<link rel="stylesheet" href="css/template.css" type="text/css" />
 	<script src="js/focus.js" type="text/javascript"></script>
-	
-    
+
+
 </head>
 
 <body>
-	<div class="mobMenuOverlay"></div> 
-	<header class="site-header shade"><a href="/" class="router-link-exact-active router-link-active" id="logo">BVTH</a> 
+	<div class="mobMenuOverlay"></div>
+	<header class="site-header shade"><a href="/" class="router-link-exact-active router-link-active" id="logo">BVTH</a>
 	<div id="mainmenu">
-		<div class="mobMenuHeader">Menu</div> 
+		<div class="mobMenuHeader">Menu</div>
 		<ul class="menu"><li><a href="/" class="router-link-exact-active router-link-active">Home</a></li> <li><a href="#" class="">About</a></li> <li><a href="#" class="">News</a></li> <li><a href="#" class="">Tasks</a></li> <li><a href="#" class="">Contact</a></li>
-		</ul> 
+		</ul>
 		<div class="mobMenuFooter">Volg me ook op hyves!</div>
-	</div> 
-	<div id="mobMenuBtn"><span class="mobMenuLine"></span></div> 
+	</div>
+	<div id="mobMenuBtn"><span class="mobMenuLine"></span></div>
 	<div class="clearfix"></div>
-	</header> 
-	<div id="home"><div class="imgHolder bgteaser"><div class="imgData"><h1>Vue.js website</h1></div></div> 
+	</header>
+	<div id="home"><div class="imgHolder bgteaser"><div class="imgData"><h1>Boilerplate website</h1></div></div> 
 	<div class="centered">
 		<div class="content">
-			<p class="lead">Test website in Vue.js met api koppelingen, router en meta-data.</p> 
-			<hr> 
-			<a href="#" class="btn">Lees verder</a> 
-			
+			<p class="lead">Test website in Vue.js met api koppelingen, router en meta-data.</p>
+			<hr>
+			<a href="#" class="btn">Lees verder</a>
+
 			<div class="well media">
 				<div class="pull-left">
 					<div class="imgHolder imgDefault"></div>
-				</div> 
+				</div>
 				<div class="media-body">
-					<h3 class="media-heading">Bert van 't Hul</h3> 
-					<div  class="media-text">Full-stack developer</div> 
+					<h3 class="media-heading">Bert van 't Hul</h3>
+					<div  class="media-text">Full-stack developer</div>
 					<a href="#" class="btn">Lees meer...</a>
 				</div>
 			</div>
